4b06c5ed0bf5554a2ea014e0f3649193227b3c51
Anthony Castillo <anthonycastillo821@gmail.com>
2021-06-04 21:55:44 -0500

    Heroku sync

---
+++ client/build/precache-manifest.0b15192807ed943720152508c62948ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33db1dbc1249770b3f3bf3a96d387bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0335d55dc95dd8b1454",
+    "url": "/static/css/main.18b8a792.chunk.css"
+  },
+  {
+    "revision": "8fd5523befa9c13317c2",
+    "url": "/static/js/2.956aeca7.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.956aeca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0335d55dc95dd8b1454",
+    "url": "/static/js/main.2d32be9e.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
